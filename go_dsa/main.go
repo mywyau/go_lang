fb1a135dff8b9af1b588a263bb58da09c06b35df
michael-hmrc <47248104+michael-hmrc@users.noreply.github.com>
2025-12-07 16:22:09 +0000

    wip

--- go_dsa/main.go
+++ go_dsa/main.go
@@ -2,9 +2,15 @@ package main
 
 import (
 	"fmt"
+
 	"github.com/mywyau/go_lang/go_dsa/basics"
+	"github.com/mywyau/go_lang/go_dsa/searching"
 )
 
 func main() {
+	
+	arr := []int{1, 3, 5, 7, 9, 11}
+
     fmt.Println(basics.X, basics.Name, basics.Alive)
+    fmt.Println(searching.Binary_search(arr, 7))
 }
