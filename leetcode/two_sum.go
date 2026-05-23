1c9d86b01b534669c879febeddbf7c917fc8cb3c
michael-hmrc <47248104+michael-hmrc@users.noreply.github.com>
2026-05-23 21:55:17 +0100

    add two sum tests

---
+++ leetcode/two_sum.go
@@ -0,0 +1,17 @@
+package leetcode
+
+func twoSum(nums []int, target int) []int {
+	seen := map[int]int{}
+
+	for i, num := range nums {
+		needed := target - num
+
+		if previousIndex, found := seen[needed]; found {
+			return []int{previousIndex, i}
+		}
+
+		seen[num] = i
+	}
+
+	return nil
+}
---
+++ leetcode/two_sum_test.go
@@ -0,0 +1,18 @@
+package leetcode
+
+import "testing"
+
+func TestTwoSum(t *testing.T) {
+	got := twoSum([]int{2, 7, 11, 15}, 9)
+	want := []int{0, 1}
+
+	if len(got) != len(want) {
+		t.Fatalf("expected %v, got %v", want, got)
+	}
+
+	for i := range want {
+		if got[i] != want[i] {
+			t.Fatalf("expected %v, got %v", want, got)
+		}
+	}
+}
