8641cd54ed9d9ba4817e94b5cc861077e2e8aa44
michael-hmrc <47248104+michael-hmrc@users.noreply.github.com>
2026-05-24 00:42:05 +0100

    add more go examples

--- dsa/searching/binarySearch.go
+++ dsa/searching/binarySearch.go
@@ -1,23 +1,21 @@
 package searching
 
-// capitalisation to allow export
-// lowercase binarySearch would make this function private
-
 func BinarySearch(nums []int, target int) int {
-
 	left, right := 0, len(nums)-1
 
 	for left <= right {
-
 		mid := left + (right-left)/2
 
 		if nums[mid] == target {
 			return mid
-		} else if nums[mid] < target {
+		}
+
+		if nums[mid] < target {
 			left = mid + 1
 		} else {
 			right = mid - 1
 		}
 	}
-	return -1 // not found
+
+	return -1
 }
---
+++ leetcode/searching/search_insert.go
@@ -0,0 +1,28 @@
+package searching
+
+// This is LeetCode 35.
+// Given a sorted array, return the index of the target. If it does not exist, return where it should be inserted.
+
+func SearchInsert(nums []int, target int) int {
+	left, right := 0, len(nums)-1
+
+	for left <= right {
+		mid := left + (right-left)/2
+
+		if nums[mid] == target {
+			return mid
+		}
+
+		if nums[mid] < target {
+			left = mid + 1
+		} else {
+			right = mid - 1
+		}
+	}
+
+	return left
+}
+
+
+// Why return left?
+// Because when the loop finishes, left points to the correct insert position.
