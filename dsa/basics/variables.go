42b9807c17e5affb85ef4ac7a74fed72abe45c5e
michael-hmrc <47248104+michael-hmrc@users.noreply.github.com>
2026-05-24 01:24:14 +0100

    variables

--- dsa/basics/variables.go
+++ dsa/basics/variables.go
@@ -4,3 +4,19 @@ var X int = 10
 var Name string = "John"
 var Alive bool = true
 
+// common var types
+var name string = "Michael"
+var age int = 30
+var price float64 = 19.99
+var active bool = true
+
+// short form vars in functions not allowed in package level
+func shortForm() {
+	name := "Michael"
+	age := 30
+	isDeveloper := true
+
+	println(name)
+	println(age)
+	println(isDeveloper)
+}
