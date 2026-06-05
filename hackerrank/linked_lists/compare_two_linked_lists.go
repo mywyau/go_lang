0a80fe59308b77a95fa338c6f1c56cfe76ce1984
michael-hmrc <47248104+michael-hmrc@users.noreply.github.com>
2026-06-05 23:54:07 +0100

    add compareLinkedLists

---
+++ hackerrank/linked_lists/compare_two_linked_lists.go
@@ -0,0 +1,14 @@
+package linkedlists
+
+func compareLists(llist1 *SinglyLinkedListNode, llist2 *SinglyLinkedListNode) bool {
+	for llist1 != nil && llist2 != nil {
+		if llist1.data != llist2.data {
+			return false
+		}
+
+		llist1 = llist1.next
+		llist2 = llist2.next
+	}
+
+	return llist1 == nil && llist2 == nil
+}
