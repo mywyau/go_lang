9a8d06ba1e2b8711842f9e7a382db44dad77e6b5
michael-hmrc <47248104+michael-hmrc@users.noreply.github.com>
2026-06-06 12:16:39 +0100

    wip

--- hackerrank/linked_lists/insert_node_at_tail.go
+++ hackerrank/linked_lists/insert_node_at_tail.go
@@ -1,17 +1,32 @@
 package linkedlists
 
+// insertNodeAtTail adds a new node to the end of a linked list.
+//
+// Plain English idea:
+// - Create a new node with the given data.
+// - If the list is empty, the new node becomes the head.
+// - Otherwise, walk through the list until we reach the last node.
+// - Point the last node's next to the new node.
+// - Return the original head.
+//
+// Example:
+//
+//	Current list: 10 -> 20 -> 30
+//	Insert 40 at tail
+//	Result:       10 -> 20 -> 30 -> 40
+
 func insertNodeAtTail(head *SinglyLinkedListNode, data int32) *SinglyLinkedListNode {
-    newNode := &SinglyLinkedListNode{data: data}
+	newNode := &SinglyLinkedListNode{data: data}
 
-    if head == nil {
-        return newNode
-    }
+	if head == nil {
+		return newNode
+	}
 
-    current := head
-    for current.next != nil {
-        current = current.next
-    }
+	current := head
+	for current.next != nil {
+		current = current.next
+	}
 
-    current.next = newNode
-    return head
+	current.next = newNode
+	return head
 }
