26fcf44edb32e79a1aab1465463aefff27eb9b3b
michael-hmrc <47248104+michael-hmrc@users.noreply.github.com>
2026-06-06 16:26:52 +0100

    wip

--- hackerrank/arrays/rotate_left.go
+++ hackerrank/arrays/rotate_left.go
@@ -34,6 +34,23 @@ package arrays
 // 	return result
 // }
 
+/*
+	Main idea:
+
+	A left rotation moves the first d items to the end of the array.
+
+	Example:
+	arr = [1, 2, 3, 4, 5]
+	d = 2
+
+	Move the first 2 items to the end:
+
+	[3, 4, 5] + [1, 2] = [3, 4, 5, 1, 2]
+
+	We use d % len(arr) so extra full rotations do not matter.
+	For example, rotating 5 items by 5 gives the same array.
+*/
+
 func rotateLeft(d int32, arr []int32) []int32 {
 	n := len(arr)
 
@@ -42,12 +59,18 @@ func rotateLeft(d int32, arr []int32) []int32 {
 	}
 
 	// Convert d from int32 to int because slice indexes need int
+	// we use modulo because for larger number of rotations which are greater than the array ends up resetting the array,
+	//  and we need to find the remainder == number of actual rotations
 	rotations := int(d) % n
 
-	result := []int32{}
-	result = append(result, arr[rotations:]...)
+	result := []int32{}  // empty slice to build the answer
+
+	// The ... in Go is like saying: take all the items from this slice and append them one by one.
+	//  e.g. arr = [1, 2, 3, 4, 5],  d = 2 this would append [3,4,5] to the empty result
+	result = append(result, arr[rotations:]...) 
+	
+	// then we append [1,2] to [3,4,5] = [3,4,5,1,2]
 	result = append(result, arr[:rotations]...)
 
-	return result
+	return result // which results in the array rotated to the left by 2
 }
-
