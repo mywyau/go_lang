3bf2c86b64f619786c88233dad0e004097270206
michael-hmrc <47248104+michael-hmrc@users.noreply.github.com>
2026-06-07 01:02:01 +0100

    add getheight function

---
+++ hackerrank/trees/getHeight.go
@@ -0,0 +1,18 @@
+package trees 
+
+
+func getHeight(root *TreeNode) int {
+
+	if root == nil {
+		return -1
+	}
+
+	leftHeight := getHeight(root.left)
+	rightHeight := getHeight(root.right)
+
+	if leftHeight > rightHeight {
+		return leftHeight + 1
+	}
+	
+	return rightHeight + 1
+}
