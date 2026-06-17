4dab523be880ca5bcc6e4fe57b1599cb547143e8
michael-hmrc <47248104+michael-hmrc@users.noreply.github.com>
2026-06-17 18:34:26 +0100

    birthdatcand;les

--- hackerrank/algorithms/warmup/birthday_cake_candles.go
+++ hackerrank/algorithms/warmup/birthday_cake_candles.go
@@ -1,14 +1,18 @@
 package warmup
 
+
+// Find the maximum value in an array, then count how many times it appears. 
+
 func birthdayCakeCandles(candles []int32) int32 {
-	tallest := candles[0]
-	count := int32(0)
+
+	tallest := candles[0]   // initialise the tallest candle to be the initial value in the array as a guess
+	count := int32(0)		// set the count for the number of tallest candles to be 0 at the beginning
 
 	for _, candle := range candles {
-		if candle > tallest {
+		if candle > tallest {		// if the candle is taller than the current percieved tallest we need to update tallest and reset the count to one   
 			tallest = candle
 			count = 1
-		} else if candle == tallest {
+		} else if candle == tallest {   // we add the tallest candle to the count
 			count++
 		}
 	}
