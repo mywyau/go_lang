1b56c74e541460319cfdd557526960f16afa801c
michael-hmrc <47248104+michael-hmrc@users.noreply.github.com>
2026-06-23 11:15:43 +0100

    wip

--- hackerrank/algorithms/warmup/a_very_big_sum.go
+++ hackerrank/algorithms/warmup/a_very_big_sum.go
@@ -1,5 +1,10 @@
 package warmup
 
+// we first define a sum as int64
+// then we iterate as normal thorugh the array, adding to the sum 
+// finally we return the sum 
+// done
+
 func aVeryBigSum(ar []int64) int64 {
 
 	var sum int64 = 0
@@ -10,3 +15,5 @@ func aVeryBigSum(ar []int64) int64 {
 
 	return sum
 }
+
+
