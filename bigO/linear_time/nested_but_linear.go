c507af61e2513b2e76719949476d8a97ced39a2e
michael-hmrc <47248104+michael-hmrc@users.noreply.github.com>
2026-06-29 12:49:58 +0100

    add notes on nested but linear

--- bigO/linear_time/nested_but_linear.go
+++ bigO/linear_time/nested_but_linear.go
@@ -2,6 +2,8 @@ package bigo
 
 import "fmt"
 
+// this is a nested loop but the inner loop only runs  10 times hence not quadratic since there is constant number of iterations in the inner loop
+
 func printTenThingsForEach(nums []int) {
 	for _, num := range nums {
 		for i := 0; i < 10; i++ {
