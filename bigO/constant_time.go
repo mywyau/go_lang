3f7060e109c41a4347276af9b3646a6314624d3a
michael-hmrc <47248104+michael-hmrc@users.noreply.github.com>
2026-06-29 12:48:21 +0100

    wip

--- bigO/constant_time.go
+++ bigO/constant_time.go
@@ -7,6 +7,9 @@ package bigo
 // As the input gets bigger, how does the work grow?
 // It does not usually mean exact speed in seconds. It means the growth pattern.
 
+// this will always be O(1) since it just accesses a single element 
+// even with 1, 10, 100, 1000, 10000, 100000, 1000000 etc. 
+
 func firfirstElement(nums []int) int {
 	return nums[0]
 }
--- bigO/quadratic_time.go
+++ bigO/quadratic_time.go
@@ -2,6 +2,10 @@ package bigo
 
 import "fmt"
 
+// often nested loops but not necessarily
+// if nums has 5 items, 5 x 5 = 25 pairs
+// if nums has 10 items then it's 100 x 100 = 10,000 pairs
+
 func printPairs(nums []int) {
 	for _, a := range nums {
 		for _, b := range nums {
