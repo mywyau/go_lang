1e1804c4dca99e67e7e7fd0c35a8d7ef0b965ee2
michael-hmrc <47248104+michael-hmrc@users.noreply.github.com>
2026-07-15 22:31:07 +0100

    wip

--- leetcode/searching/two_sum.go
+++ leetcode/searching/two_sum.go
@@ -8,7 +8,25 @@ func TwoSum(nums []int, target int) []int {
 	for i, num := range nums {
 		needed := target - num
 
+		// this syntax combines the map look up with the if condition
 		if previousIndex, found := seen[needed]; found {
+			return []int{previousIndex, i}
+		}
+
+		seen[num] = i
+	}
+
+	return nil
+}
+
+func TwoSumDiffSyntax(nums []int, target int) []int {
+	seen := map[int]int{}
+
+	for i, num := range nums {
+		needed := target - num
+
+		previousIndex, found := seen[needed]
+		if found {
 			return []int{previousIndex, i}
 		}
 
